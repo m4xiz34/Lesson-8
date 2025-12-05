04ec6264b112d802205b16867811e7adf5598aca
m4xiz34 <maxisacroissant@gmail.com>
2025-12-05 19:41:08 +1100

    Create maths suck.py

---
+++ maths suck.py
@@ -0,0 +1,11 @@
+mean1=38
+wrong_number = 36
+correct_number = 56
+total_number = 40
+sum = mean1*total_number
+print("The sum of 40 numbers", sum)
+num2=sum-(wrong_number-correct_number)
+print(num2)
+
+mean2 = num2/total_number
+print(mean2)
